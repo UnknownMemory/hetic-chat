6dcc3291ba536f86f6acf5e7ad23215c1829c5a8
Jérémy <jeremyschneider21@gmail.com>
2023-12-02 20:18:28 +0100

    update app setup

--- api/app.py
+++ api/app.py
@@ -1,11 +1,18 @@
+import os
+
 from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
+from dotenv import load_dotenv
+
 app = Flask(__name__)
+
+load_dotenv('./api/.env.local') if app.debug else load_dotenv()
+app.config['SQLALCHEMY_DATABASE_URI'] = str(os.getenv('POSTGRES_URL'))
+
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 db = SQLAlchemy(app)
-
 
 @app.route("/api/python")
 def hello_world():
